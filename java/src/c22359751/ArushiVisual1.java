e3114639504558ad3d74b704a82d6cc1de260e92
arushisingh2803 <124163610+arushisingh2803@users.noreply.github.com>
2024-03-30 13:54:28 +0000

    heart animation

--- java/src/c22359751/ArushiVisual1.java
+++ java/src/c22359751/ArushiVisual1.java
@@ -33,14 +33,11 @@ public class ArushiVisual1 extends Visual
         loadAudio("java/data/meetmehalfway.mp3");     
     }
 
-    float smoothedBoxSize = 0;
-
     public void draw() {
         calculateAverageAmplitude();
         background(0);
-        lights();
-        stroke(map(getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
-        fill(map(getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
+        stroke(map(getSmoothedAmplitude(), 0, 1, 0, 255), 180, 360);
+        fill(map(getSmoothedAmplitude(), 0, 1, 0, 255), 180, 360);
         camera(0, 0, 0, 0, 0, -1, 0, 1, 0);
         translate(0, 0, -250);
 
@@ -48,9 +45,9 @@ public class ArushiVisual1 extends Visual
         
         //first half of the heart
         beginShape();
-        for (float a = PI; a < 0; a += 0.01) {
+        for (float a = 0; a < PI; a += 0.01) {
             float r = 8;
-            float x = r * 16 * pow(sin(a), 3) * scale;
+            float x = r * 16 * pow(sin(a), 3) * scale + (map(getSmoothedAmplitude(), 0, 1, 0, 255));
             float y = -r*(13 * cos(a) - 5*cos(2*a) - 2*cos(3*a) - cos(4*a)) * scale;
             vertex (x, y);
         }
@@ -60,7 +57,7 @@ public class ArushiVisual1 extends Visual
         beginShape();
         for (float a = 0; a < PI; a += 0.01) {
             float r = 8;
-            float x = r * 16 * pow(sin(a), 3) * scale + 20*scale;
+            float x = -r * 16 * pow(sin(a), 3) * scale - (map(getSmoothedAmplitude(), 0, 1, 0, 255));
             float y = -r*(13 * cos(a) - 5*cos(2*a) - 2*cos(3*a) - cos(4*a)) * scale;
             vertex (x, y);
         }
