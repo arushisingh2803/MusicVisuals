b6957066a640b9bef6368e48ec3f522af125363d
arushisingh2803 <124163610+arushisingh2803@users.noreply.github.com>
2024-04-24 14:22:39 +0100

    last min changes

--- java/src/c22359751/ArushiVisual2.java
+++ java/src/c22359751/ArushiVisual2.java
@@ -8,6 +8,7 @@ public class ArushiVisual2 extends PApplet {
 
     CombinedVisual cv;
     int numLines = 30;
+    PImage albumCover; // Declare albumCover variable here
 
     // Constructor that takes a parameter of type CombinedVisual
     public ArushiVisual2(CombinedVisual cv) {
@@ -27,10 +28,12 @@ public class ArushiVisual2 extends PApplet {
     }
 
     public void setup() {
-        cv.colorMode(HSB);
-        cv.setFrameSize(256);
+        cv.background(0);
+        cv.colorMode(HSB, 360, 100, 100);
         cv.startMinim();
         cv.loadAudio("java/data/meetmehalfway.mp3");
+        albumCover = cv.loadImage("java/data/cover.png"); // Load the albumCover image here
+        albumCover.resize(cv.width * 2 / 3, cv.height);
     }
 
     float angleOffset = 0;
@@ -38,11 +41,11 @@ public class ArushiVisual2 extends PApplet {
     public void render() { 
         cv.calculateAverageAmplitude();
         cv.background(0);
-    
+
         float amplitude = cv.getSmoothedAmplitude();
-    
+
         cv.blendMode(ADD);
-    
+
         // Lines moving in a circle
         cv.strokeWeight(2);
         cv.colorMode(HSB, 360, 100, 100);
@@ -50,30 +53,30 @@ public class ArushiVisual2 extends PApplet {
         for (int i = 0; i < numLines; i++) {
             float angle = angleOffset + i * angleStep;
             float radius = min(cv.width, cv.height) * 4;
-    
+
             float x1 = cv.width / 2;
             float y1 = cv.height / 2;
             float x2 = x1 + cos(angle) * radius;
             float y2 = y1 + sin(angle) * radius;
-    
+
             float hue = map(i, 0, numLines, 0, 360);
             cv.stroke(hue, 80, 80);
             cv.line(x1, y1, -x2, -y2);
-    
+
             angleOffset += cv.getSmoothedAmplitude();
         }
-    
+
         cv.blendMode(BLEND);
-    
+
         float hue = map(amplitude, 0, 1, 0, 360);
         cv.pushMatrix();
         cv.translate(0, 0, 1);
         cv.imageMode(CENTER);
         cv.tint(hue, 180, 180);
-        // Load and resize the album cover using cv
-        PImage albumCover = cv.loadImage("java/data/cover.png");
-        albumCover.resize(cv.width * 2 / 3, cv.height);
-        cv.image(albumCover, cv.width / 2, cv.height / 2);
+        // Render the album cover image if it's not null
+        if (albumCover != null) {
+            cv.image(albumCover, cv.width / 2, cv.height / 2);
+        }
         cv.popMatrix();
     }
 }
